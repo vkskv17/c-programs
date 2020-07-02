5c87249658e31197c6c6895474f40def1bf8abe6
Vinay Kumar Sharma <vkskv17@gmail.com>
2020-07-02 20:21:28 +0530

    Added 2 new programs.

---
+++ wordCount.c
@@ -0,0 +1,34 @@
+
+#include<stdio.h>
+
+#define IN 1    //Inside a word.
+#define OUT 0   //Outside a word.
+
+// Program counts lines, words and characters in input.
+
+int main(){
+
+	int c, nl, nw,nc, state;
+
+	state = OUT;
+	nl  = nw = nc = 0;
+
+	while ((c = getchar()) != EOF){
+		++nc;
+
+		if (c == '\n')
+			++nl;
+
+		if (c == ' ' || c == '\n' || c == '\t')
+			state = OUT;
+
+		else if (state == OUT){
+			state = IN;
+			++nw;
+		}
+
+	}
+
+	printf("%d  %d  %d\n", nl, nw, nc);
+
+}
